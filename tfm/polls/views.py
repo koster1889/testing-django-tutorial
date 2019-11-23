def8ed83329ef225fcfc946de5e643f80cf5d74f
Johannes Andersson <johannes.andersson@jeppesen.com>
2019-11-23 23:16:14 +0100

    Added django template for polls, easy peasy!

--- tfm/polls/views.py
+++ tfm/polls/views.py
@@ -1,5 +1,28 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("HelloWorld. This is polls!")
+    questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'questions': questions
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+
+
+def detail(request, question_id):
+        return HttpResponse("Question: %s." % question_id)
+
+def results(request, question_id):
+    response = "results for question %s " % question_id
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse("Youre voting on question %s." % question_id)
