f7093028f38f8e3ce4ab945402f38d83e627fbe8
Johannes Andersson <johannes.andersson@jeppesen.com>
2020-03-15 09:32:41 +0100

    added options to create poll

--- tfm/polls/urls.py
+++ tfm/polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('create', views.create, name='create'), #GET?
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:pk>/', views.DetailView.as_view(), name='details'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
--- tfm/polls/views.py
+++ tfm/polls/views.py
@@ -5,11 +5,13 @@ from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
-from .models import Question
+from .models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -43,3 +45,31 @@ def vote(request, question_id):
     selected_choice.save()
 
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def create(request):
+    nr_or_poll_options = 5
+    
+    try:
+        new_poll = request.POST
+    except:
+        new_poll = None
+    if new_poll:
+        new_question = Question(question_text=new_poll['question-text'])
+        options = []
+        
+        for i in range(nr_or_poll_options):
+            option = new_poll['choice-' + str(i)]
+            if option:
+                options.append(option)
+
+        new_question.pub_date = timezone.now()
+        new_question.save()
+
+        for option in options:
+            new_choice = Choice(choice_text=option, question=new_question)
+            new_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:details', args=(new_question.id,)))
+    else:
+        return render(request, 'polls/create.html', {'poll_options_list': range(nr_or_poll_options)})
